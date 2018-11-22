aa601d099543026e8da6652b09c798a9c349191a
user <matthew@erg.abdn.ac.uk>
2018-11-22 15:28:31 +0000

    Comments are super

--- probe.c
+++ probe.c
@@ -14,17 +14,17 @@
 
 uint64_t count = 0;
 struct bin {
-	unsigned char hash[20];//20
-	unsigned char hash_sub[20]; //20
-	uint32_t mss_quic;//4
+	unsigned char hash[20];//20	SHA1 of the IP
+	unsigned char hash_sub[20]; //20	SHA1 of the subnet. IPv4/24 IPv6/48
+	uint32_t mss_quic;//4		Records the TCP MSS value or the UDP payload bytes that correspond to a QUIC version id
 	uint32_t segment_size;//4
-	uint8_t dscp;//1
-	uint8_t opts;//1
-	unsigned char trans;//1
-	uint16_t ip4 : 1;
-	uint16_t mf : 1;
-	uint16_t df : 1;
-	uint16_t ecn_ns : 1;
+	uint8_t dscp;//1		Differentiated Services Code Point
+	uint8_t opts;//1		The position of MSS in the options list
+	unsigned char trans;//1		T-cp or U-dp
+	uint16_t ip4 : 1;//bitflags	is ip4?
+	uint16_t mf : 1;//		More fragments
+	uint16_t df : 1;//		Don't fragment
+	uint16_t ecn_ns : 1;//bitflags	9 TCP flags aka Control bits
 	uint16_t fin : 1;
 	uint16_t syn : 1;
 	uint16_t rst : 1;
@@ -39,7 +39,6 @@ unsigned char salt2[30];
 
 static void per_packet(libtrace_packet_t *packet, FILE *f, struct bin *record)
 {
-	//trace_set_capture_length(packet, 120); //snaplen
 	uint16_t ether;
 	void *ip_hdr;
 	uint32_t rem;
@@ -51,57 +50,50 @@ static void per_packet(libtrace_packet_t *packet, FILE *f, struct bin *record)
 	libtrace_ip6_t *ip6;
 	void *hdr;
 	uint8_t dscp;	
-//	char str[INET6_ADDRSTRLEN];
 	SHA_CTX context;
 	SHA1_Init(&context);
 	switch(ether) {
 		case TRACE_ETHERTYPE_IP:
 			ip = (libtrace_ip_t *)ip_hdr;
 			record->ip4 = 1;
-			////printf("IP %s ", inet_ntop(AF_INET, &(ip->ip_src.s_addr), str, sizeof(str)));
 			hdr = trace_get_payload_from_ip(ip, &proto, &rem);
 			dscp = (ip->ip_tos);
-			//record->opts = (ip->ip_off)>>5;
-			record->mf = 1&((ip->ip_off)>>5);
+			record->mf = 1&((ip->ip_off)>>5);//extract from 16bit fragment offset and flags field
 			record->df = 1&((ip->ip_off)>>6);
-	//		printf("%d %d\n", ip->ip_off, record->opts);
-			uint32_t ad = ntohl(ip->ip_src.s_addr);
+			uint32_t ad = ntohl(ip->ip_src.s_addr);//hash the whole IP address
 			SHA1_Update(&context, &ad, sizeof(uint32_t));
 			SHA1_Update(&context, salt1, 30);
-			SHA1_Final(record->hash, &context);//build
+			SHA1_Final(record->hash, &context);
 			
-			ad = ad>>8;
+			ad = ad>>8;//hash the /24 subnet
 			SHA1_Init(&context);
-			SHA1_Update(&context, &ad, sizeof(uint32_t));//additional hash of /24
+			SHA1_Update(&context, &ad, sizeof(uint32_t));
 			SHA1_Update(&context, salt2, 30);
-			SHA1_Final(record->hash_sub, &context);//build
+			SHA1_Final(record->hash_sub, &context);
 			break;
 		case TRACE_ETHERTYPE_IPV6:
 			ip6 = (libtrace_ip6_t *)ip_hdr;
 			record->ip4 = 0;
-			////printf("IP6 %s ", inet_ntop(AF_INET6, &(ip6->ip_src.s6_addr), str, sizeof(str)));
 			dscp = ntohl(ip6->flow)>>20;
 			dscp &= 255;
 			hdr = trace_get_payload_from_ip6(ip6, &proto, &rem);
 			
-			SHA1_Update(&context, (ip6->ip_src.s6_addr),16);
+			SHA1_Update(&context, (ip6->ip_src.s6_addr),16);//hash the whole IP6 address
 			SHA1_Update(&context, salt1, 30);
-			SHA1_Final(record->hash, &context);//build
+			SHA1_Final(record->hash, &context);
 
 			unsigned char *ad6 = malloc(6);
-			memcpy(ad6, (ip6->ip_src.s6_addr), 6);
-			//printf("ip6: %d ip6# %d\n", *ad6, *ip6->ip_src.s6_addr);
+			memcpy(ad6, (ip6->ip_src.s6_addr), 6);//hash the /48 subnet
 			SHA1_Init(&context);
 			SHA1_Update(&context, ad6, 6);
 			SHA1_Update(&context, salt2, 30);
-			SHA1_Final(record->hash_sub, &context);//build
+			SHA1_Final(record->hash_sub, &context);
 			
 			break;
 
 		default:
 			return;
 	}
-	//printf("DSCP: %d ", dscp);
 	libtrace_tcp_t *tcp;
 	libtrace_udp_t *udp;
 	unsigned char type, optlen, *data, *opt_ptr;
@@ -109,36 +101,30 @@ static void per_packet(libtrace_packet_t *packet, FILE *f, struct bin *record)
 	switch(proto) {
 		case TRACE_IPPROTO_TCP:
 			tcp = (libtrace_tcp_t *)hdr;
-			//dscp |= 1<<7; //set top bit if TCP
-			record->dscp = dscp;//build
-			//printf("TCP ");	
+			record->dscp = dscp;
 			record->trans = 'T';
 			if (ether == TRACE_ETHERTYPE_IP) {
-				//printf("payload: %d ", ntohs(ip->ip_len) - 4*(tcp->doff + ip->ip_hl));
-				record->segment_size = ntohs(ip->ip_len) - 4*(tcp->doff + ip->ip_hl);//build
-
+				record->segment_size = ntohs(ip->ip_len) - 4*(tcp->doff + ip->ip_hl);
 			} else {
-
-				//printf("payload: %d ", ntohs(ip6->plen)- tcp->doff*4);
-				record->segment_size =  ntohs(ip6->plen)- tcp->doff*4;//build
+				record->segment_size =  ntohs(ip6->plen)- tcp->doff*4;
 			}
 			len = tcp->doff * 4 - sizeof(libtrace_tcp_t);
 			if (len == 0) break;
 
 			opt_ptr = (unsigned char *)tcp + sizeof(libtrace_tcp_t);
 			uint8_t n = 0;
+			uint32_t mss = 0;
 			while(trace_get_next_option(&opt_ptr, &len, &type, &optlen, &data)) {
 				n++;
 				if (type != 2) break;
-				uint32_t mss = 0;
 				for(int k = 0; k < optlen-2; k++){
 					mss |= (data[k])<<(k*8);
 				}
 			mss = ntohs(mss);
-			record->mss_quic = mss;//build
-			record->opts = n;
+			record->mss_quic = mss;//absence of mss option is recorded as mss == 0 and n == 0
+			record->opts = n;	//an mss of 0 is stupid but recorded as mss == 0 and n >= 1. read_stats.c may need to be modified if this case occurs and needs to be filtered from the results
 			}
-			record->ecn_ns = tcp->ecn_ns;
+			record->ecn_ns = tcp->ecn_ns;	//bunch of TCP flags
 			record->fin = tcp->fin;
 			record->syn = tcp->syn;
 			record->rst = tcp->rst;
@@ -149,42 +135,27 @@ static void per_packet(libtrace_packet_t *packet, FILE *f, struct bin *record)
 			record->cwr = tcp->cwr;
 			break;
 		case TRACE_IPPROTO_UDP:
-			//printf("UDP ");
-			//dscp |= 1<<6; //set 2nd top bit if UDP
 			record->trans = 'U';
-			record->dscp = dscp;//build
+			record->dscp = dscp;
 			udp = (libtrace_udp_t *)hdr;
 			char *payload = trace_get_payload_from_udp(udp, &rem);
 			if ( rem == 0) return;
 			if (ntohs(udp->len)<21) return;
 			uint32_t pay = (uint32_t)payload[12]<<24 | (uint32_t)payload[11]<<16 | (uint32_t)payload[10]<<8 | (uint32_t)payload[9];
-			//printf("quic: 0x%02x%02x%02x%02x ", (uint8_t)payload[9],(uint8_t) payload[10], (uint8_t)payload[11], (uint8_t)payload[12]);
-			//printf("mangled quic: 0x%x ", pay);
-			//printf("payload: %d ", ntohs(udp->len)-8);
-			record->mss_quic = pay;//build
+			record->mss_quic = pay;//Assuming the payload were a QUIC packet, these 4 bytes refer to the version id field
 			record->segment_size = ntohs(udp->len)-8;
 			break;
-
 		default:
-			//printf("un protocol");
+			//Don't care about not TCP or UDP. Leaves most of the fields blank
 			return;
 	}
-
-	//printf("DSCP_proto: %d ", dscp);
-
 }
 
 static void libtrace_cleanup(libtrace_t *trace, libtrace_packet_t *packet) {
-
-        /* It's very important to ensure that we aren't trying to destroy
-         * a NULL structure, so each of the destroy calls will only occur
-         * if the structure exists */
         if (trace)
                 trace_destroy(trace);
-
         if (packet)
                 trace_destroy_packet(packet);
-
 }
 
 void cleaner(int sig) {
@@ -197,8 +168,7 @@ int main(int argc, char *argv[])
 
         libtrace_t *trace = NULL;
         libtrace_packet_t *packet = NULL;
-	int snaplen = 150;
-	int promisc = 1;
+	int snaplen = 150; //only collects first 150 bytes of each packet
 
         if (argc < 2) {
                 fprintf(stderr, "Usage: %s inputURI\n", argv[0]);
@@ -225,13 +195,12 @@ int main(int argc, char *argv[])
 		libtrace_cleanup(trace, packet);
 		return 1;
 	}
-	//trace_config(trace, TRACE_OPTION_PROMISC, &promisc); //cant set for pcapfiles
         if (trace_start(trace) == -1) {
                 trace_perror(trace,"Starting trace");
                 libtrace_cleanup(trace, packet);
                 return 1;
         }
-	FILE *s = fopen("salt1", "r");
+	FILE *s = fopen("salt1", "r");//reads the two salt files for the IP and subnet salting
 	fread(salt1, 30, 1, s);
 	fclose(s);
 	s = fopen("salt2", "r");
@@ -239,7 +208,7 @@ int main(int argc, char *argv[])
 	fclose(s);
 	char str_time[24];
 	time_t now = time (0);
-	strftime (str_time, sizeof(str_time), "%Y-%m-%d_%H:%M:%S.bin", gmtime(&now));
+	strftime (str_time, sizeof(str_time), "%Y-%m-%d_%H:%M:%S.bin", gmtime(&now));//format time string for the binary logging filename
 	
 	FILE *f = fopen(str_time, "wb");
 	if(f == NULL) {
@@ -253,15 +222,13 @@ int main(int argc, char *argv[])
 	record = malloc(sizeof(struct bin));
         while (trace_read_packet(trace,packet)>0) {
                 per_packet(packet, f, record);
-		//printf(" dscp: %d", record->dscp);
-		//printf("\n");
 		
 		count++;
-		if(count > 1000000){ //every million packets rotate logs. appropriate number? 31MB logs
+		if(count > 1000000){ //every million packets rotate logs. 56MB logs. update the dropped packet stats
 			fclose(f);
 			now = time(0);
 			strftime (str_time, sizeof(str_time), "%Y-%m-%d_%H:%M:%S.bin", gmtime(&now));
-			snprintf(str_dropped, 100, "%s d%lu r%lu a%lu\n", str_time, trace_get_dropped_packets(trace), trace_get_received_packets(trace),trace_get_accepted_packets(trace));
+			snprintf(str_dropped, 100, "%s D%lu R%lu A%lu\n", str_time, trace_get_dropped_packets(trace), trace_get_received_packets(trace),trace_get_accepted_packets(trace));
 			fwrite(str_dropped, strlen(str_dropped), 1, stats);	
 			fflush(stats);
 			f = fopen(str_time, "wb");
@@ -272,8 +239,7 @@ int main(int argc, char *argv[])
 			count = 0;
 		}
 		fwrite(record, sizeof(struct bin), 1, f);
-		//fflush(f);
-		memset(record->hash, 1, sizeof(record->hash));
+		memset(record->hash, 1, sizeof(record->hash));//clear the record after writing, not all fields are used for every packet so attempt to avoid noise from old data
 		memset(record->hash_sub, 1, sizeof(record->hash_sub));
 		record->mss_quic = 0; record->segment_size = 0; record->dscp = 0; 
 		record->opts = 0; record->trans='X';
